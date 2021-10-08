2042b8d0b6b25864826f05e41de382d2900b69d7
BuddhiChathurika <92119094+BuddhiChathurika@users.noreply.github.com>
2021-10-08 13:08:35 +0530

    Added divide function

--- file1.py
+++ file1.py
@@ -4,7 +4,11 @@ def add(num1, num2):
 def subtract(num1, num2):
     return num1 - num2
 
+def divide(num1, num2):
+    return num1 / num2
+
 
 print("Please select operation -\n"
       "1. Add\n"
-      "2. Subtract\n")
+      "2. Subtract\n"
+      "3. Divide\n")
