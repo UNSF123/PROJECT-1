6167bbd3bb9d5e369ab4e569391def63281af969
Nithya Sonali <nithyasonali1@gmail.com>
2021-10-08 12:25:14 +0530

    create new code

--- file2.py
+++ file2.py
@@ -1,1 +1,6 @@
- 
+from numpy import add, subtract, divide
+
+select = int(input("Select operations form 1, 2, 3 :"))
+
+number_1 = int(input("Enter first number: "))
+number_2 = int(input("Enter second number: "))
